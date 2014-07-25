01b8edd0df7b6ae4d4f5116a3da4eb572914568e
mfw <mfw@mfwtekiMacBook-Pro.local>
2014-07-25 15:05:01 +0800

    测试帐号

---
+++ TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  DrawRectDemo
+//
+//  Created by mfw on 14-7-25.
+//  Copyright (c) 2014年 MaFengWo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIViewController
+
+@end
---
+++ TestView.m
@@ -0,0 +1,42 @@
+//
+//  TestView.m
+//  DrawRectDemo
+//
+//  Created by mfw on 14-7-25.
+//  Copyright (c) 2014年 MaFengWo. All rights reserved.
+//
+
+#import "TestView.h"
+
+@interface TestView ()
+
+@end
+
+@implementation TestView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    /**
+     *  测试帐号
+     *
+     *  @return 测试提交登录
+     */
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
